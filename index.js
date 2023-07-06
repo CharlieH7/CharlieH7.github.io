7d08bac863194df70fab41aba88dae04e042adbf
CharlieH7 <hope91824@gmail.com>
2023-07-06 23:23:52 +1000

    shapes in the background on mouse hovering:

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let isModalOpen = false;
 let isContrastOn = false;
+const scaleFactor = 1 / 20;
 const currentYear = new Date().getFullYear();
 document.getElementById("year").innerHTML = currentYear;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+  }
+
 function toggleContrast() {
     isContrastOn = !isContrastOn;
     if (isContrastOn) {
