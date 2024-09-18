ea2520a0443ef08e1f57ce16e5b44d5fefb6cc77
Charlie Huang <hope91824@gmail.com>
2024-09-18 12:31:29 +1000

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Wizards from "@/components/models/Wizards";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
-      <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25"/>
+      <Image src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-15"/>
       <div className="w-full h-screen">
         {/* navigation and 3d model */}
         <RenderModel>
